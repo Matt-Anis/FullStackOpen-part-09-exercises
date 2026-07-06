56caf9d40a2d49fc7a31eb96e01880f457e7fd8a
Matt Anis <webdev.omis@gmail.com>
2026-07-06 16:48:14 +0100

    17. Course, step1

---
+++ course/src/components/Content.tsx
@@ -0,0 +1,22 @@
+type CoursePart = {
+  name: string;
+  exerciseCount: number;
+};
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+const Content = ({ courseParts }: ContentProps) => {
+  return (
+    <div>
+      {courseParts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Content;
